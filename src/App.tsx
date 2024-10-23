01536d6f5e58ccf9d5a188e14b14a4d988d4f88c
felipe-autentique <felipe@autentique.com.br>
2024-10-22 21:38:48 -0300

    Toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import { useState } from 'react'
 import { Button } from '@/components/ui/button'
+import { ThemeProvider } from './providers/theme-provider'
+import { ThemeToggle } from './components/theme-toggle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMRaa
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <div className="bg-background min-h-screen font-sans antialiased">
+
+                <ThemeToggle/>
+
+                <Button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+                </Button>
+                <p>
+                Edit <code>src/App.tsx</code> and save to test HMRaa
+                </p>
+
+            </div>
+
+        </ThemeProvider>
     </>
   )
 }
