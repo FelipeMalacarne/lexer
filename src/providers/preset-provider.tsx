3b2dc95177268604bbac410a81f805b232b1801e
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-11-26 18:35:05 -0300

    preset provider refactor to sync with localstorage

--- src/providers/preset-provider.tsx
+++ src/providers/preset-provider.tsx
@@ -1,10 +1,18 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
 import { Preset, presets as presetData } from "@/lib/presets"; // Adjust the import path as necessary
+import { v4 as uuidv4 } from "uuid"; // Install uuid using: npm install uuid
 
 interface PresetsContextType {
   selectedPreset: Preset;
-  setSelectedPreset: (id: Preset) => void;
+  setSelectedPreset: (preset: Preset) => void;
   presets: Preset[];
+  addPreset: (name: string, tokens: string[]) => void;
 }
 
 const PresetsContext = createContext<PresetsContextType | undefined>(undefined);
@@ -21,13 +29,54 @@ interface PresetsProviderProps {
   children: ReactNode;
 }
 
-export const PresetsProvider: React.FC<PresetsProviderProps> = ({ children }) => {
-  const [selectedPreset, setSelectedPreset] = useState<Preset>(
-    presetData[0]
-  );
+export const PresetsProvider: React.FC<PresetsProviderProps> = ({
+  children,
+}) => {
+  const localStorageKey = "presets";
+
+  const [presets, setPresets] = useState<Preset[]>(() => {
+    const storedPresets = localStorage.getItem(localStorageKey);
+    return storedPresets ? JSON.parse(storedPresets) : presetData;
+  });
+
+  const [selectedPreset, setSelectedPreset] = useState<Preset>(() => {
+    const storedPresets = localStorage.getItem(localStorageKey);
+    const initialPresets: Preset[] = storedPresets
+      ? JSON.parse(storedPresets)
+      : presetData;
+    return (
+      initialPresets.find(
+        (preset) => preset.name.toLowerCase() === "default"
+      ) || initialPresets[0]
+    );
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(presets));
+  }, [presets]);
+
+  const addPreset = (name: string, tokens: string[]) => {
+    if (
+      presets.some((preset) => preset.name.toLowerCase() === name.toLowerCase())
+    ) {
+      alert("Preset with this name already exists.");
+      return;
+    }
+
+    const newPreset: Preset = {
+      id: uuidv4(),
+      name,
+      tokens,
+    };
+
+    setPresets((prevPresets) => [...prevPresets, newPreset]);
+    setSelectedPreset(newPreset);
+  };
 
   return (
-    <PresetsContext.Provider value={{ selectedPreset, setSelectedPreset, presets: presetData }}>
+    <PresetsContext.Provider
+      value={{ selectedPreset, setSelectedPreset, presets, addPreset }}
+    >
       {children}
     </PresetsContext.Provider>
   );
